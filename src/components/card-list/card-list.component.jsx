5329641e556e29d425f1cec5a521a0cc11cdbec1
themis1986 <themis_1986@hotmail.com>
2022-08-16 17:21:32 +0300

    Add styles in the components

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,13 +1,24 @@
 import { Component } from "react";
+import "./card-list.styles.css";
 
 class CardList extends Component {
   render() {
     const { monsters } = this.props;
-    
+
     return (
-      <div className="">
+      <div className="card-list">
         {monsters.map((monster) => {
-          return <h1 key={monster.name}>{monster.name}</h1>;
+          const { name, email, id } = monster;
+          return (
+            <div className="card-container">
+              <img
+                src={`https://robohash.org/${id}?set=set2&size=180x180`}
+                alt={`monster ${name}`}
+              />
+              <h2>{name}</h2>
+              <p>{email}</p>
+            </div>
+          );
         })}
       </div>
     );
