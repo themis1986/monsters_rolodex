c6be52f0109e29e7dccc40e612aa35ad03bbad57
themis1986 <themis_1986@hotmail.com>
2022-08-17 12:34:52 +0300

    Refactor all class components to functional components

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,19 +1,16 @@
-import { Component } from "react";
 import Card from "../card/card.component";
 import "./card-list.styles.css";
 
-class CardList extends Component {
-  render() {
-    const { monsters } = this.props;
+const CardList = ({ monsters }, forwardRef) => {
+  //  const { monsters } = props;
 
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => {
-          return <Card monster={monster} key={monster.id} />;
-        })}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="card-list">
+      {monsters.map((monster) => {
+        return <Card monster={monster} key={monster.id} />;
+      })}
+    </div>
+  );
+};
 
 export default CardList;
